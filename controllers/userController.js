ed94807af7c941e3a866859b82c0d03ac7955fa1
jeevanthask <jeevanthanandika@gmail.com>
2023-09-12 20:40:21 +0530

    jwt middleware for protected routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.addUser = (req, res) => {
   const user = new User(req.body.username, req.body.email, req.body.password);
   user
     .saveUser()
     .then((result) => {
-      res.send(result);
+      const token = jwt.sign({ username: req.body.username }, "nandikasecret");
+      res.header("x-auth-token", token).send(result);
     })
     .catch((error) => {
       res.send(error);
@@ -21,7 +23,11 @@ exports.logUser = (req, res) => {
         .compare(req.body.password, result[0][0].password)
         .then((result) => {
           if (result) {
-            res.send("user authenticated");
+            const token = jwt.sign(
+              { username: req.body.username },
+              "nandikasecret"
+            );
+            res.send(token);
           } else {
             res.send("Unauthorized user");
           }
